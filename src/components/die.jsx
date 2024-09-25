9f18cb6a2dc2abeda5cffe5cb2a202cd03747ae3
cruzrafaelx <rafaelcruz@outlook.de>
2024-09-25 03:52:46 +0200

    create Die component and manually render

---
+++ src/components/die.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Die({value}){
+      return(
+      
+      <div className="die--body">
+            <h2 className="die--num">{value}</h2>
+      </div>
+)}
